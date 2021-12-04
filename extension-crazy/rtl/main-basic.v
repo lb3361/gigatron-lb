44b4bec77488e4fc4c20e964910a6de3bc3f3bf6
lb3361 <lb3361@nyu.edu>
2021-12-03 23:28:29 -0500

    initial verilog compile

---
+++ extension-crazy/rtl/main-basic.v
@@ -0,0 +1,92 @@
+
+module main(
+    input            CLK,
+    input            CLKx2,
+    input            CLKx4,
+    output reg [7:0] OUTD,
+    input [7:0]      ALU,
+    input            nOL,
+    output           nAE,
+    output [18:0]    RA,
+    input [7:0]      RDIN,
+    output [7:0]     RDOUT,
+    output           nROE,
+    output           nRWE,
+    input [15:0]     GA,
+    input [7:0]      GBUSIN,
+    output [7:0]     GBUSOUT,
+    input            nGOE,
+    input            nGWE,
+    output           nACTRL,
+    output [1:0]     nADEV,
+    output reg       SCK,
+    input            MISO,
+    output reg       MOSI,
+    output reg [1:0] nSS,
+    inout [4:3]      XIN );
+
+   reg               SCLK;
+   reg               nZPBANK;
+   reg [1:0]         BANK;
+   wire              nSCTRL;
+   
+   /* OUT Register (like a 74HC377) */
+   always @(posedge CLK)
+     begin
+        if (!nOL)
+          OUTD <= ALU;
+     end
+
+   /* IO25 used purely as input.
+    * This could also be used to output 
+    * a signal for debugging purposes */
+   assign XIN = 2'bZ;
+   
+   /* 74HCT244 always open.
+    * Otherwise one could do something like:
+    *   always @(negedge CLKx2)  nAE <= !CLK; */
+   assign nAE = 1'b0;
+   
+   /* Ram address bus */
+   wire zpswap = !nZPBANK && ( GA[14:7] == 8'b00000001 );
+   wire bankenable = GA[15] ^ zpswap;
+   wire [3:0] gabank = (bankenable) ? { 2'b00, BANK } : { 4'b0000 };
+   assign RA = { gabank, GA[14:0] };
+
+   /* Ram data bus */
+   assign RDOUT = GBUSIN;
+   
+   /* Gigatron bus out */
+   wire       addr0 = (GA == 16'h0000);   
+   wire       portenable = SCLK && addr0;
+   assign GBUSOUT = (portenable) ? { BANK, XIN, 3'b000, MISO } : RDIN;
+   
+   /* Ram control */
+   assign nROE = nGOE | portenable;
+   assign nRWE = nGWE | !nGOE;
+   
+   /* Ctrl detection */
+   assign nSCTRL = nGOE || nGWE || GA[3:2] == 2'b00;
+   assign nACTRL = nGOE || nGWE || GA[3:2] != 2'b00;
+   assign nADEV[0] = GA[7:4] == 4'b0000;
+   assign nADEV[1] = GA[7:4] == 4'b0000;
+   
+   /* Ctrl bits */
+   always @(negedge CLK)
+     if (!nSCTRL) 
+       begin
+          MOSI <= GA[15];
+          BANK <= GA[7:6];
+          nZPBANK <= GA[5];
+          nSS <= GA[3:2];
+          SCLK <= GA[0];
+          SCK <= GA[0] ^~ GA[4];
+       end
+   
+endmodule 
+
+/* Local Variables: */
+/* indent-tabs-mode: () */
+/* End: */
+
+            
---
+++ extension-crazy/rtl/top.v
@@ -0,0 +1,92 @@
+
+module top(
+    input         CLK,
+    input         CLKx2,
+    input         CLKx4,
+    input         nGOE,
+    output [7:0]  OUTD,
+    input [7:0]   ALU,
+    input         nOL,
+    inout [7:0]   RAL,
+    output [18:8] RAH,
+    output        nROE,
+    output        nRWE,
+    inout [7:0]   RD,
+    output        nAE,
+    inout [7:0]   GBUS,
+    input [15:8]  GAH,
+    input         nGWE,
+    output        nACTRL,
+    output [1:0]  nADEV,
+    inout [4:3]   XIN,
+    input [2:0]   MISO,
+    output        MOSI,
+    output        SCK,
+    output [1:0]  nSS
+);
+
+   /* This toplevel module isolates
+      all the programming of inout pins
+      where a mistake could potentially
+      result in a short. */
+      
+   wire [18:0]       RA;
+   wire [15:0]       GA;
+   wire [7:0]        GBUSOUT;
+   wire [7:0]        RDOUT;
+
+   /* Ram Address bus
+    * This prevents from driving the low address lines
+    * from both the CPLD and the 74HCT244 */
+   assign RAL = (nAE) ? RA[7:0] : 8'bZ; 
+   assign RAH = RA[18:8];
+   
+   /* Gigatron address bus.
+    * The lower half is valid when nAE==0 only. */
+   assign GA = { GAH, (nAE) ? 8'bX : RAL };
+   
+   /* Gigatron data bus.
+    * This prevents from driving it when nGOE=1 */
+   assign GBUS = (nGOE) ? 8'bZ : GBUSOUT;
+   
+   /* Ram data bus. This prevents from driving it when nROE=1 */
+   assign RD = (nROE) ? 8'bZ : RDOUT;
+   
+   /* MISO mixing */
+   wire              MISOm;
+   assign MISOm = (MISO[0] & !nSS[0]) | 
+                  (MISO[1] & !nSS[1]) | 
+                  (MISO[2] & nSS[0] & nSS[1]);
+
+   /* Invoke main module */
+   main themain( 
+		 .CLK (CLK),
+                 .CLKx2 (CLKx2),
+                 .CLKx4 (CLKx4), 
+                 .OUTD (OUTD),
+                 .ALU (ALU),
+                 .nOL (nOL),
+                 .nAE (nAE),
+                 .RA (RA),
+                 .RDIN (RD),
+                 .RDOUT (RDOUT),
+                 .nROE (nROE),
+                 .nRWE (nRWE),
+                 .GA (GA),
+                 .GBUSIN (GBUS),
+                 .GBUSOUT (GBUSOUT),
+                 .nGOE (nGOE),
+                 .nGWE (nGWE),
+                 .nACTRL (nACTRL),
+                 .nADEV (nADEV),
+                 .SCK (SCK),
+                 .MISO (MISOm),
+                 .MOSI (MOSI),
+                 .nSS (nSS),
+                 .XIN (XIN) );
+
+endmodule
+
+/* Local Variables: */
+/* indent-tabs-mode: () */
+/* End: */
