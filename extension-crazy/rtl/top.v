00484a20f5db52aa3b9fd1f37800acf7b330ac9f
lb3361 <lb3361@nyu.edu>
2022-01-05 10:06:47 -0500

    top works (incomplete though)

--- extension-crazy/rtl/top.v
+++ extension-crazy/rtl/top.v
@@ -1,6 +1,6 @@
 
 
-module top(input CLK,
+module top(input            CLK,
            input            CLKx2,
            input            CLKx4,
            input            nGOE,
@@ -30,12 +30,11 @@ module top(input CLK,
    (* PWR_MODE = "LOW" *) reg [1:0]   BANK;
    (* PWR_MODE = "LOW" *) reg [3:0]   BANK0R;
    (* PWR_MODE = "LOW" *) reg [3:0]   BANK0W;
-   (* PWR_MODE = "LOW" *) reg [4:0]   VBANK;
-   (* PWR_MODE = "LOW" *) reg [15:0]  VADDR;
-   (* PWR_MODE = "STD" *) reg [15:0]  GA;
-   (* PWR_MODE = "STD" *) reg         nBE;
+   (* PWR_MODE = "LOW" *) reg [3:0]   VBANK;
+   (* PWR_MODE = "STD" *) reg [15:0]  VADDR;
 
-   /*  Clocks
+   /* ================ Clocks
+    *
     *                              110000000000111111000000000011111100
     *                              450123456789012345012345678901234501
     *                               _____           _____           ___
@@ -48,36 +47,30 @@ module top(input CLK,
     *                                 _   _   _   _   _   _   _   _   _
     *  CLKx4                       \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \
     *                              ____         _______         _______
-    *  /AE                             \_______/       \_______/       \
-    *                                  ^ downedge(CLKX4) & CLKx2 & CLK
-    *                                          ^ downedge(CLKx4) & CLKx2 & /CLK
-    *                                   ___________     ___________     
-    *  /BE                         \___/           \___/           \___/
-    *                                              ^ downedge(CLKx4) & /CLKx2 & /AE
+    *  /BE                             \_______/       \_______/       \
+    *                               _______         _______         ____
+    *  /AE                         /       \_______/       \_______/    
+    *
+    *  Cycle                       --VVV-vvvGGGGGGGG-VVV-vvvGGGGGGGG-VVV
     */
+   
+   (* PWR_MODE = "STD" *) reg nBE;
    always @(negedge CLKx4)
      begin
         if (CLKx2)
-          nAE <= !CLK;
-        nBE <= (CLKx2 || !nAE);
-     end
-
-   /* Gigatron address bus */
-   always @*
-     begin
-        GA[15:8] = GAH[15:8];
-        if (!nAE)               // transparent latch
-          GA[7:0] = RAL[7:0];
+          nBE <= !CLK;
+        nAE <= nBE;
      end
    
-   /* Gigatron data bus */
-   (* PWR_MODE = "LOW" *) (* KEEP = "TRUE" *) wire misox;
+   /* ================ Gigatron data bus */
+   
    (* PWR_MODE = "LOW" *) (* KEEP = "TRUE" *) wire gahz;
-   (* PWR_MODE = "LOW" *) reg [7:0] gbusout;
-   (* PWR_MODE = "LOW" *) wire portx;
-   assign misox = (MISO[0] & !nSS[0]) | (MISO[1] & !nSS[1]) | (MISO[2] & nSS[0] & nSS[1]);
    assign gahz = (GAH[14:8] == 7'h00);
+   (* PWR_MODE = "LOW" *) wire portx;
    assign portx = SCLK && !GAH[15] && gahz;
+   (* PWR_MODE = "LOW" *) wire misox;
+   assign misox = (MISO[0] & !nSS[0]) | (MISO[1] & !nSS[1]) | (MISO[2] & nSS[0] & nSS[1]);
+   (* PWR_MODE = "LOW" *) reg [7:0] gbusout;
    always @*
      if (! nAE)                 // transparent latch
        casez ( { portx, RAL[7:0] } )
@@ -85,32 +78,53 @@ module top(input CLK,
          { 1'b1, 8'hF0 } :   gbusout = { BANK0W[3:0], BANK0R[3:0] };           // bank data
          default:            gbusout = RD[7:0];                                // ram data
        endcase
-   assign GBUS = (nGOE) ? 8'bZZZZZZZZ : gbusout;
+   assign GBUS = (nGOE) ? 8'hZZ : gbusout;
    
-   /* Ram signals */
-   (* PWR_MODE = "LOW" *) wire [18:8] rahv;
-   (* PWR_MODE = "LOW" *) (* KEEP = "TRUE" *) reg [18:8] rahg;
-   wire bankenable = GAH[15] ^ (!nZPBANK && GA[7] && gahz);
+   
+   /* ================ Gigatron bank selection */
+   
+   (* PWR_MODE = "STD" *) wire bankenable;
+   assign bankenable = GAH[15] ^ (!nZPBANK && RAL[7] && gahz);
+   (* PWR_MODE = "STD" *) (* KEEP = "TRUE" *) reg [3:0] gbank;
    always @*
      casez ( { bankenable, BANK[1:0], nGOE } )
-       4'b0??? :  rahg = { 4'b0000, GAH[14:8] };            // no banking
-       4'b1000 :  rahg = { BANK0R[3:0], GAH[14:8] };        // bank0, reading
-       4'b1001 :  rahg = { BANK0W[3:0], GAH[14:8] };        // bank0, writing
-       default :  rahg = { 2'b00, BANK[1:0], GAH[14:8] };   // bank123
+       4'b0??? :  gbank = { 4'b0000 };            // no banking
+       4'b1000 :  gbank = { BANK0R[3:0] };        // bank0, reading
+       4'b1001 :  gbank = { BANK0W[3:0] };        // bank0, maybe writing
+       default :  gbank = { 2'b00, BANK[1:0] };   // bank123
      endcase 
-   assign rahv = { VBANK[3:1], VADDR[15:8] };
-   assign RAL = (nAE) ? GA[7:0] : 8'bZZZZZZZZ;  // should be: (nAE) ? VADDR[7:0] : 8'bZZZZZZZZ;
-   assign RAH = rahg[18:8];                     // should be: (nAE) ? rahv[18:8] : rahg[18:8];
-   assign nROE = nGOE && !nAE;
-   assign nRWE = nGWE || nAE || !nGOE;
-   assign RD = (nROE) ? GBUS : 8'bZZZZZZZZ;
    
-   /* Scanline detection */
+   
+   /* ================ SRAM interface 
+    *
+    * This is very tricky because we must ensure
+    * that no conflict arises when we commute the 74lvc244.
+    * The solution is to ensure when nAE rises,
+    * both the xc95144 and the 74lvc244 have the same
+    * idea of what should be on RAL.
+    */
+
+   assign nROE = 1'b0;
+   assign nRWE = nGWE || nAE || !nGOE;
+   assign RD = (nRWE) ? 8'hZZ : GBUS;
+   
+   (* PWR_MODE = "STD" *) reg [18:0] ra;
+   always @(posedge CLKx4)
+     if (nAE)
+       ra = { VBANK[3:2], VBANK[nBE], VADDR[15:0] };
+     else
+       ra = { gbank, GAH[14:8], RAL[7:0] };
+   assign RAH = (nAE) ? ra[18:8] : { gbank, GAH[14:8] };
+   assign RAL = (nAE) ? ra[7:0] : 8'hZZ;
+
+   
+   /* ================ Scanline detection */ 
+   
    (* PWR_MODE = "LOW" *) reg snoop;
    (* PWR_MODE = "LOW" *) (* KEEP = "TRUE" *) wire snoopchg = !nGOE && !(gahz && !GAH[15]);
    (* PWR_MODE = "LOW" *) (* KEEP = "TRUE" *) wire [7:0] nvaddr = VADDR[7:0] + 8'h01;
-   always @(negedge CLKx4)
-     if (!CLKx2 && !nAE)        // mid gigatron cycle
+   always @(negedge CLKx2)
+     if (! nAE)
        begin
           if (! nOL)
             // Snooping starts when an OUT instruction reads memory
@@ -118,49 +132,37 @@ module top(input CLK,
             snoop <=  snoopchg;
           if (! nOL && ! nGOE)
             // Reset snooping address when an OUT reads memory
-            VADDR <= GA;
+            VADDR <= { GAH, RAL };
           else
             // Otherwise increment address to next pixel
             VADDR[7:0] <= nvaddr;
        end
    
-   /* Video output register */
-   always @(negedge CLKx4)
-     begin
-        // sync
-        if (!CLKx2 && nAE && !nOL)
-          OUTD[7:6] = ALU[7:6];
-        // first half pixel
-        if (!CLKx2 && nAE)
-          OUTD[5:0] = (snoop) ? 6'h20 : 6'h00; // (snoop) ? RD[5:0] : 6'h00;
-     end
-   /* Video output register 
-   always @(posedge CLK)
-     begin
-        if (!nOL)
-          OUTD <= ALU;
-     end
-
-   /* Ctrl detection */
-   wire nCTRL;
-   assign nCTRL = nGOE || nGWE;
-   assign nACTRL = nCTRL || GA[3:2] != 2'b00;
-   assign nADEV[0] = (GA[7:4] == 4'b0000);
-   assign nADEV[1] = (GA[7:4] == 4'b0001);
    
-   /* Ctrl bits */
+   /* ================ Output register */
+
+   always @(posedge CLK)
+     if (! nOL)
+       OUTD <= ALU;
+   
+   /* ================ Ctrl codes */
+   
+   (* PWR_MODE = "LOW" *) wire nCTRL = nAE || nGOE || nGWE;
+   assign nACTRL =   nCTRL || RAL[3:2] != 2'b00;
+   assign nADEV[0] = nAE   || RAL[7:4] == 4'b0000;
+   assign nADEV[1] = nAE   || RAL[7:4] == 4'b0001;
    always @(posedge nCTRL)
      begin
         /* Normal ctrl code */         
-        if (GA[3:2] != 2'b00)
+        if (RAL[3:2] != 2'b00)
           begin
-             MOSI <= GA[15];
-             BANK <= GA[7:6];
-             nZPBANK <= GA[5];
-             nSS <= GA[3:2];
-             SCLK <= GA[0];
-             SCK <= GA[0] ^~ GA[4];
-             if (GA[1:0] == 2'b11) // System reset
+             MOSI <= GAH[15];
+             BANK <= RAL[7:6];
+             nZPBANK <= RAL[5];
+             nSS <= RAL[3:2];
+             SCLK <= RAL[0];
+             SCK <= RAL[0] ^~ RAL[4];
+             if (RAL[1:0] == 2'b11) // System reset
                begin
                   BANK0R[3:0] <= 4'b0;
                   BANK0W[3:0] <= 4'b0;
@@ -169,18 +171,18 @@ module top(input CLK,
           end
         /* Extended ctrl code */
         else
-          case (GA[7:4])
+          case (RAL[7:4])
             4'hf : begin        // Device 0xf : extended banking
-               BANK0R[3:0] <= GA[11:8];
-               BANK0W[3:0] <= GA[15:12];
+               BANK0R[3:0] <= GAH[11:8];
+               BANK0W[3:0] <= GAH[15:12];
             end
             4'he : begin        // Decide 0xe : set video bank
-               VBANK[3:0] <= GA[11:8];
+               VBANK[3:0] <= GAH[11:8];
             end
           endcase
      end
-
 endmodule
+
 
 /* Local Variables: */
 /* indent-tabs-mode: () */
